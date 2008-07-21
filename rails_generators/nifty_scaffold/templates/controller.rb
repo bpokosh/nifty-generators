c9b02d25975be7694ad2d780337146e477e0e0b7
Brian Pokosh <bpokosh@gmail.com>
2008-07-21 18:19:10 -0400

    fixed title method call in views, added error_messages_for to form partial

--- rails_generators/nifty_scaffold/templates/controller.rb
+++ rails_generators/nifty_scaffold/templates/controller.rb
@@ -2,4 +2,13 @@ class <%= plural_class_name %>Controller < ApplicationController
   make_resourceful do
     actions :all
   end
+  
+  def current_objects
+    @current_objects = current_model.paginate(:all,
+          :order => ‘orders.created_at’,
+          :page => params[:page],
+          :conditions => search_conditions,
+          :joins => :user)
+  end
+  
 end
